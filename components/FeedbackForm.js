42869469c4c058adeca1098d47d3c60f6cacff4b
Matthijs <matthijsvanherwijnen@gmail.com>
2024-04-30 17:41:55 +0200

    Fix

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -1,22 +1,24 @@
 import styles from './FeedbackForm.module.css'
+import { useState } from 'react';
 
 export default function FeedbackForm() {
-  const submitHandler = async (e) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const submitHandler = async () => {
     console.log(JSON.stringify(e.target));
-    await fetch("/.netlify/functions/lead", {method: 'POST', body: JSON.stringify({name: 'Kees', email: 'kees@keesie.nl'})})
+    await fetch("/.netlify/functions/lead", {method: 'POST', body: JSON.stringify({name, email})})
     window.URL = "/success"
-    e.preventDefault();
   }
   return (
-      <form onSubmit={submitHandler}>
-  
-        <label htmlFor="name">Naam</label>
-        <input id="name" className={styles['form-field']} type="text" name="name" />
+  <div>
+      <label htmlFor="name">Naam</label>
+      <input id="name" className={styles['form-field']} type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
 
-        <label htmlFor="email">Emailadres</label>
-        <input id="email" className={styles['form-field']} type="email" name="email" required />
+      <label htmlFor="email">Emailadres</label>
+      <input id="email" className={styles['form-field']} type="email" name="email" required value={email} onChange={(e) => setEmail(e.target.value)}/>
 
-        <button className={styles.button} type="submit">Draai aan het rad!</button>
-      </form>
+      <button className={styles.button} type="submit" onClick={submitHandler}>Draai aan het rad!</button>
+  </div>
   )
 }
