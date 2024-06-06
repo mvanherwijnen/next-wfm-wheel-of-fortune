2a5c75ecdb863b9f3c3c6c8b4b9f3c06c8d27838
Matthijs <matthijsvanherwijnen@gmail.com>
2024-06-06 17:21:38 +0200

    Reset email en name

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -1,5 +1,5 @@
 import styles from './FeedbackForm.module.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function FeedbackForm() {
   const [name, setName] = useState('');
@@ -19,6 +19,8 @@ export default function FeedbackForm() {
     }
     setEmailError('');
     const response = await fetch("/.netlify/functions/lead", {method: 'POST', body: JSON.stringify({name, email})})
+    setName('');
+    setEmail('');
     window.location.href = `/success?dealId=${(await response.json()).dealId}`
   }
   return (
